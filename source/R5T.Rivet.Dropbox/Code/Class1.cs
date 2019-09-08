eefe6cf3057ac8f9565a71810da41e5161e690b9
MinexAutomation <minex.automation@gmail.com>
2019-09-08 14:48:01 -0400

    Added utilities for the dropbox Rivet and Rivet/Data directories. Removed the initial class.

--- source/R5T.Rivet.Dropbox/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Rivet.Dropbox
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Rivet.Dropbox/Code/Utilities.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+using R5T.NetStandard.IO.Paths;
+using R5T.NetStandard.IO.Paths.Extensions;
+using R5T.Rivet.Organizational;
+
+using DropboxUtilities = R5T.Dropbox.Base.Utilities;
+using OrganizationalUtilities = R5T.NetStandard.Organizational.Utilities;
+
+
+namespace R5T.Rivet.Dropbox
+{
+    public static class Utilities
+    {
+        public static string RivetDirectoryPathValue
+        {
+            get
+            {
+                var dropboxDirectoryPathValue = DropboxUtilities.DropboxDirectoryPathValue;
+
+                var output = OrganizationalUtilities.AppendOrganizationsAndDefaultOrganizationDirectories(dropboxDirectoryPathValue, RivetOrganization.Instance);
+                return output;
+            }
+        }
+
+        public static DirectoryPath RivetDirectoryPath
+        {
+            get
+            {
+                var output = Utilities.RivetDirectoryPathValue.AsDirectoryPath();
+                return output;
+            }
+        }
+
+        public static string DataDirectoryPathValue
+        {
+            get
+            {
+                var output = OrganizationalUtilities.AppendDataDirectory(Utilities.RivetDirectoryPathValue);
+                return output;
+            }
+        }
+
+        public static DirectoryPath DataDirectoryPath
+        {
+            get
+            {
+                var output = Utilities.DataDirectoryPathValue.AsDirectoryPath();
+                return output;
+            }
+        }
+    }
+}
